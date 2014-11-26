7e153a877c40d51822ba3ee229b3da14a54a2634
unknown <rencie.bautista@yahoo.com>
2014-11-26 15:32:31 +0800

    update

--- app/views/home/create.blade.php
+++ app/views/home/create.blade.php
@@ -1,26 +1,19 @@
 @extends('layouts.layouts')
 
 @section('content')
-<div class="content">
- 	{{ Form::open(array('action' => 'RegisterController@store','class' => 'form-horizontal')) }}
+<div class="content text-center">
+ 	{{ Form::open(array('action' => 'RegisterController@store','class' => 'form-inline')) }}
     <div class="form-group">
-			{{ Form::label('username', 'Email address', array('class' => 'col-sm-4 control-label')) }}
-			<div class="col-sm-8">
-				{{ Form::text('username','',array('class' => 'form-control', 'placeholder' => 'Email address')) }}
-			</div>			
-		</div>
-		<div class="form-group">
-			{{ Form::label('password', 'Password', array('class' => 'col-sm-4 control-label')) }}
-			<div class="col-sm-8">
-				{{ Form::password('password',array('class' => 'form-control', 'placeholder' => 'Password')) }}
-			</div>			
-		</div>
-		
-	  	<div class="form-group">
-			<div class="col-sm-offset-4 col-sm-8">
-			  	<button type="submit" class="btn btn-default btn-xl">Login</button>
-			</div>
-	  	</div>
+    <div class="input-group">
+      <label class="sr-only" for="exampleInputEmail2">Email address</label>	
+      <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="sr-only" for="exampleInputPassword2">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-default btn-xl">Login</button>
     {{ Form::close() }}
 </div>
 @stop

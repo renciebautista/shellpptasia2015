9d5614e0f4651cdc14d78ec3c088a7514360fb9b
unknown <rencie.bautista@yahoo.com>
2014-12-02 12:02:04 +0800

    updaye

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'shell',
+			'username'  => 'shelluser',
+			'password'  => '031988',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
--- app/views/onepage/index.blade.php
+++ app/views/onepage/index.blade.php
@@ -2,28 +2,32 @@
 
 @section('navigation')
   <ul class="nav nav-pills pull-right">
-    <li  class="active"><a href="/">Home</a></li>
-    <li ><a href="/programme">Programme</a></li>
-    <li ><a href="/hotel">Hotel</a></li>
-     <li><a href="/register">Event Registration</a></li>
+	<li  class="active"><a href="/">Home</a></li>
+	<li ><a href="/programme">Programme</a></li>
+	<li ><a href="/hotel">Hotel</a></li>
+	 <li><a href="/register">Event Registration</a></li>
   </ul>
 @stop
 
 @section('content')
 <div class="row">
 	<div class="col-lg-12">
-	    <p>Building on last year’s themes – the energy-water-food stress nexus and the role resilience can play in addressing future resource stresses – this edition of <strong>Shell Powering Progress Together Asia</strong> will focus on <strong>Resilience in an Urbanising World</strong> and the key role cities will play to be liveable and sustainable.  This is a significant topic for Asia since it is the most populous continent, and home to over half of the world’s megacities.</p>
-	            <p>This one-day event will include four interactive sessions:</p>
-	            <ul>
-	                <li><strong>Resilience: Current Thinking and Trends</strong> – creates a deeper understanding of resilience and urbanisation in the face of the stress nexus, with special focus on the challenges and opportunities for Asian cities;</li>
-	                <li><strong>Case Studies on Resilience for Cities</strong> – discusses how innovative and eco-conscious urban planning can transform cities into smart hubs for sustainable progress; </li>
-	                <li><strong>Implications on Resilience for Business</strong> - explores how resource scarcity is helping business and civil society work in collaboration to design strategies to be resilient; and</li>
-	                <li><strong>Inspiring Stories of Innovation and Resilience</strong> - shares stories of innovative disaster mitigation and management as well as rehabilitation in response to climate risks </li>
-	            </ul>
-	            <p>Former <strong>Bloomberg and current Aljazeera broadcast journalist Maryam Nemazee</strong> and <strong>Rappler CEO Maria Ressa</strong> will moderate the forum.</p>
-	            <p>The event once again gathers some 300 of thought leaders from business, government and civil society to engage in a dialogue, address resiliency through multi-sector collaboration, and explore on-the-ground actions.</p>
-	            <p><strong>Shell Powering Progress Together Asia 2015</strong> once more coincides with <strong>Shell Eco-marathon Asia</strong>, a unique event bringing together hundreds of student teams from countries across Asia and the Middle East to design, build, test and compete in self-built cars to travel a set distance on the least amount of fuel – in the process pushing the boundaries of fuel efficiency.  Delegates of Powering Progress Together Asia will have the opportunity to visit the Shell Eco-marathon Asia paddocks and Fan Zone area.</p>
-	        
+		<p>Building on last year’s themes – the energy-water-food stress nexus and the role resilience can play in addressing future resource stresses – this edition of <strong>Shell Powering Progress Together Asia</strong> will focus on <strong>Resilience in an Urbanising World</strong> and the key role cities will play to be liveable and sustainable.  This is a significant topic for Asia since it is the most populous continent, and home to over half of the world’s megacities.</p>
+		<p>This one-day event will include four interactive sessions:</p>
+		<ul>
+			<li><strong>Resilience: Current Thinking and Trends</strong> – creates a deeper understanding of resilience and urbanisation in the face of the stress nexus, with special focus on the challenges and opportunities for Asian cities;</li>
+			<li><strong>Case Studies on Resilience for Cities</strong> – discusses how innovative and eco-conscious urban planning can transform cities into smart hubs for sustainable progress; </li>
+			<li><strong>Implications on Resilience for Business</strong> - explores how resource scarcity is helping business and civil society work in collaboration to design strategies to be resilient; and</li>
+			<li><strong>Inspiring Stories of Innovation and Resilience</strong> - shares stories of innovative disaster mitigation and management as well as rehabilitation in response to climate risks </li>
+		</ul>
+		<p>Former <strong>Bloomberg and current Aljazeera broadcast journalist Maryam Nemazee</strong> and <strong>Rappler CEO Maria Ressa</strong> will moderate the forum.</p>
+		<p>The event once again gathers some 300 of thought leaders from business, government and civil society to engage in a dialogue, address resiliency through multi-sector collaboration, and explore on-the-ground actions.</p>
+		<p><strong>Shell Powering Progress Together Asia 2015</strong> once more coincides with <strong>Shell Eco-marathon Asia</strong>, a unique event bringing together hundreds of student teams from countries across Asia and the Middle East to design, build, test and compete in self-built cars to travel a set distance on the least amount of fuel – in the process pushing the boundaries of fuel efficiency.  Delegates of Powering Progress Together Asia will have the opportunity to visit the Shell Eco-marathon Asia paddocks and Fan Zone area.</p>
+		
+		<ul>
+			<li><a href="https://www.youtube.com/watch?v=7jKg_gh_fBs">Watch the Shell Powering Progress Together – Asia 2014 Lookback Film</a></li>
+			<li><a href="http://www.shell.com/global/aboutshell/events/powering-progress-together/what-is-ppt.html">Know more about Shell Powering Progress Together</a></li>
+		</ul>
 	</div>
 </div>
 @stop

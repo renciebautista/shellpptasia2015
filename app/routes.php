e44005970cc9ddaddc01e7537e7f136d1f7f3498
unknown <rencie.bautista@yahoo.com>
2014-12-03 19:27:49 +0800

    udpate

--- app/routes.php
+++ app/routes.php
@@ -28,9 +28,9 @@
 Route::get('/login', 'OnePageController@login');
 Route::get('/logout', 'OnePageController@logout');
 Route::post('/login', 'OnePageController@dologin');
-Route::get('/mail', function(){
+Route::get('/mail/{id}', function(){
 	return View::make('emails.registration.confirm');
-	DNS1D::getBarcodePNGPath("ABCD1234", "C39",2,60);
+	DNS1D::getBarcodePNGPath($id, "C39",2,60);
 	// Mail::send('emails.registration.confirm', array('pathToFile' => 'www.shellpptasia.com/barcode/abcd1234.png'), function($message)
 	// {
 	//     $message->to('rencie.bautista@yahoo.com', 'John Smith')->subject('Welcome!');

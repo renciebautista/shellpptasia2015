c4582ad23e07165bb24121d6859b463d8809f559
unknown <rencie.bautista@yahoo.com>
2015-02-17 16:10:05 +0800

    add export to gms

--- app/controllers/ExportController.php
+++ app/controllers/ExportController.php
@@ -29,6 +29,14 @@ class ExportController extends \BaseController {
 
 	public function gms()
 	{
-		
+		$attendees = Attendee::getAttendee();
+		$data = json_decode(json_encode((array) $attendees), true);
+		// print_r($data);
+		Excel::create('Attendee_'.date('Ymd'), function($excel) use($data){
+			$excel->sheet('Attendee', function($sheet) use($data) {
+				$sheet->fromArray($data);
+			});
+
+		})->export('xls');
 	}
 }
--- app/models/Attendee.php
+++ app/models/Attendee.php
@@ -19,4 +19,17 @@ class Attendee extends \Eloquent {
 		}
 		
 	}
+
+	public static function getAttendee(){
+		return DB::select(DB::raw("select code as idno, last_name as lastname, 
+			first_name as firstname, prefered_name as nickname, 
+		    job_title as titled, company as company, 
+		    ' ' as aread, ' ' as schedule, ' ' as seqno, ' ' as tabled,
+		    countries.full_name as countryd, 
+			' ' as industryd, address as address1,' ' as address2,' ' as address3,
+		    mobile_phone as telno, ' ' as faxno,
+		    email as email, ' ' as website, ' ' as bday, ' ' as age, ' ' as walkin 
+		from attendees 
+		inner join countries on attendees.country_id = countries.id"));
+	}
 }
